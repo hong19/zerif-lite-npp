590180b68a3d67364adf77316856e99b5ef47669
hong19 <jimmy600kimo@hotmail.com>
2015-07-25 22:49:34 +0800

    [feature] template_introduction: change the representation of blog browsing to the specific article

--- template-introduction.php
+++ template-introduction.php
@@ -25,41 +25,31 @@ endif;
 
 	<div class="container">
 
-		<div class="content-left-wrap col-md-9">
+		<div class="content-left-wrap col-md-12">
 
 			<div id="primary" class="content-area">
 
 				<main id="main" class="site-main" role="main">
-					<?php
-					$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-					$wp_query = new WP_Query( array('post_type' => 'post', 'showposts' => '8', 'paged' => $paged) );
 
-					if( $wp_query->have_posts() ):
-					 
-						while ($wp_query->have_posts()) : 
-						
-							$wp_query->the_post();
-							get_template_part( 'content', get_post_format() );
 
-						endwhile;
 
-					endif;
+					<?php while ( have_posts() ) : the_post(); ?>
 
-					zerif_paging_nav();
 
-					wp_reset_postdata();
-					?>
+
+						<?php get_template_part( 'content', 'page' ); ?>
+
+
+
+					<?php endwhile; // end of the loop. ?>
+
+
+
 				</main><!-- #main -->
 
 			</div><!-- #primary -->
 
 		</div><!-- .content-left-wrap -->
-
-		<div class="sidebar-wrap col-md-3 content-left-wrap">
-
-			<?php get_sidebar(); ?>
-
-		</div><!-- .sidebar-wrap -->
 
 
 	</div><!-- .container -->
